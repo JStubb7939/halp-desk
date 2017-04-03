e74ac6beb634ad381e376cc11133b981a6a88776
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-03 03:47:30 -0400

    Add Heroku database connection to db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pg = require('pg');
 
-const db = 'postgres://localhost:5432/halpdesk';
+const db = 'postgres://kpmnviiofyzapp:2053cedd53d88143b95663388863e02178736fd341ece48293ba05abbdc292ea@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d973qpmit5epft', {'dialect':'postgres'};
+
+// const db = 'postgres://localhost:5432/halpdesk';
 
 const onConnect = (err, client, done) => {
   if (err) {
