7bc2d5f21c6172a2066202240c93e45b3acefee7
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-04-03 03:52:50 -0400

    Refactor heroku creds

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pg = require('pg');
 
-const db = 'postgres://kpmnviiofyzapp:2053cedd53d88143b95663388863e02178736fd341ece48293ba05abbdc292ea@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d973qpmit5epft', {'dialect':'postgres'};
+const db = "postgres://kpmnviiofyzapp:2053cedd53d88143b95663388863e02178736fd341ece48293ba05abbdc292ea@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d973qpmit5epft?ssl=true", {"dialect":"postgres", "ssl":true, "dialectOptions":{"ssl":{"require":true}}};
 
 // const db = 'postgres://localhost:5432/halpdesk';
 
